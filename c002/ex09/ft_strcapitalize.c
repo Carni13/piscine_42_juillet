615c2ce24c818130f6e603829c246c1940a2b7e1
Carni13 <83639459+Carni13@users.noreply.github.com>
2021-07-07 04:47:52 +0200

    Update ft_strcapitalize.c

--- c002/ex09/ft_strcapitalize.c
+++ c002/ex09/ft_strcapitalize.c
@@ -1,4 +1,9 @@
-char *ft_strlowcase(char *str)
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void ft_strlowcase(char *str)
 {
 	int i;
 	i = 0;
@@ -8,22 +13,57 @@ char *ft_strlowcase(char *str)
 			str[i] += 32;
 		i ++;
 	}
-	return(str);
+}
+
+int	ft_str_is_alpha(char c)
+{
+
+		if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')))
+		{
+			return (0);
+		}
+	return (1);
 }
 
 char *ft_strcapitalize(char *str)
 {
 	int i;
+
 	i = 0;
-	
-	while (str + i)
+
+	ft_strlowcase(str);
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
+
+	i++;
+	while (str[i])
 	{
+		if((ft_str_is_alpha(str[i])== 1 )&& (ft_str_is_alpha(str[i-1]) == 0))
+		{
+			if(str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+		}
+		i++;
+		}
 
-	if (str[i] > 96 && str[i] < 127) //min en maj
-			str[i] -= 32;
+	return (str);
 
-	if (str[i] > 64 && str[i] < 91) // maj en min
-			str[i] += 32;
-	}
+}
 
+int main (){
+
+	char source[]=("salut, comment tu vas? 42mots quarante-deux; cinquante+et+un");
+	char* p_src = source;
+
+	printf("%s \n",source);
+
+	ft_strcapitalize(p_src);
+
+
+	printf("%s\n",source);
+
+
+
+return 0;
 }
